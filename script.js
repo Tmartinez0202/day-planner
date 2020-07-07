7d62e463bbe8c74ad3da35a8edc01a04bdf65777
OkieKenobi <travismartinez88@gmail.comgit config --global user.name Tmartinez0202>
2020-07-06 19:40:47 -0600

    added hour blocks and classes from css

--- script.js
+++ script.js
@@ -4,29 +4,30 @@ $(document).ready(function(){
         var value = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
         localStorage.setItem(time, value);
-
+        
     })
     function hourUpdater() {
         var currentHour = moment().hours();
         $(".time-block").each(function(){
             var blockHour = parseInt($(this).attr("id").split("-")[1]);
             if(blockHour < currentHour){
-              //class should be set as past  
-            }else if(blockHour === currentHour){
-                //remove past class then replace with present class
-
+                $("textarea").addClass("past") 
+            }else if(blockHour ===  currentHour){
+                $("textarea").removeClass("past").addClass("present")
+                
             }else{
+                $("textarea").removeClass("present").removeClass("past").addClass("future")
                 //remove past and present and add future
             }
         })
-
+        
     }
     hourUpdater()
     var interval = setInterval(hourUpdater, 15000);
-
+    
     
     $("#hour-9 .description").val(localStorage.getItem("hour-9"));
     
-
+    
     
 })
