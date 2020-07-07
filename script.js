ff36ba9494f64ec8a954492879573db416c6a8d2
OkieKenobi <travismartinez88@gmail.comgit config --global user.name Tmartinez0202>
2020-07-06 20:46:59 -0600

    added local storage saving for all the days and fixed the classes for past present and future to change color

--- script.js
+++ script.js
@@ -10,14 +10,19 @@ $(document).ready(function(){
         var currentHour = moment().hours();
         $(".time-block").each(function(){
             var blockHour = parseInt($(this).attr("id").split("-")[1]);
+            console.log("blockHour", blockHour);
+            console.log("currentHour", currentHour);
             if(blockHour < currentHour){
-                $("textarea").addClass("past") 
+                console.log(("past"))
+                $(this).addClass("past") 
             }else if(blockHour ===  currentHour){
-                $("textarea").removeClass("past").addClass("present")
+                console.log("present")
+                $(this).removeClass("past").addClass("present")
                 
             }else{
-                $("textarea").removeClass("present").removeClass("past").addClass("future")
-                //remove past and present and add future
+                console.log("future");
+                $(this).removeClass("present").removeClass("past").addClass("future")
+            
             }
         })
         
@@ -27,6 +32,18 @@ $(document).ready(function(){
     
     
     $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+    $("#hour-18 .description").val(localStorage.getItem("hour-18"));
+    $("#hour-19 .description").val(localStorage.getItem("hour-19"));
+    $("#hour-20 .description").val(localStorage.getItem("hour-20"));
+    $("#hour-21 .description").val(localStorage.getItem("hour-21"));
     
     
     
